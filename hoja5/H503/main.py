3109614a039823e57ed7a5654162eb27b31d911c
Angela Serrano <aserranocu@gmail.com>
2019-11-13 12:07:55 +0100

    Ejercicio H503 finalizado

--- hoja5/H503/main.py
+++ hoja5/H503/main.py
@@ -2,6 +2,12 @@ from simulacion import Simulacion
 
 simulacion = Simulacion()
 
+def comprobarValores(valor):
+    if valor == "O" or valor == "A" or valor == "D":
+        return True
+    else:
+        return False
+
 numeroPiezas = int(input("¿Con cuantas piezas vamos a jugar? "))
 while numeroPiezas >7 or numeroPiezas <3:
     print("Número no valido. Solo de 3 a 7.")
@@ -10,4 +16,18 @@ while numeroPiezas >7 or numeroPiezas <3:
 simulacion.setNumeroPiezas(numeroPiezas)
 simulacion.vistaInicial()
 
-
+while simulacion.juegoFinalizado() == False:
+    origen = (input("¿De qué torre cogemos la pieza? ¿O, A, D? ")).upper()
+    while comprobarValores(origen) == False:
+        print("Introduzca un valor válido")
+        origen = input("¿De qué torre cogemos la pieza? ¿O, A, D? ").upper()
+        
+    destino = input("¿En qué torre la dejamos? ").upper()
+    while comprobarValores(destino) == False:
+        print("Introduzca un valor válido")
+        destino = input("¿En qué torre la dejamos? ").upper()
+        
+    simulacion.moverPieza(origen,destino)
+    simulacion.mostrarTorres()
+    
+print("¡LO CONSEGUISTE!")
--- hoja5/H503/simulacion.py
+++ hoja5/H503/simulacion.py
@@ -9,26 +9,33 @@ class Simulacion:
     global punteroD
 
     def __init__(self):
-        self.punteroA = 0
-        self.punteroD = 0
+        self.punteroO = -1
 
     def setNumeroPiezas(self, numeroPiezas):
         self.numeroPiezas = numeroPiezas
         self.punteroA = numeroPiezas-1
+        self.punteroD = numeroPiezas-1
     
     def vistaInicial(self):
         self.torreO = ["" for i in range(0,self.numeroPiezas)]
         self.torreA = ["" for i in range(0,self.numeroPiezas)]
         self.torreD = ["" for i in range(0,self.numeroPiezas)]
-
+        
         for i in range (0, self.numeroPiezas):
-            self.torreO[i] = " "*(self.numeroPiezas-i-1) + "*"*(2*i+1) + " "*(self.numeroPiezas-i-1)
-            self.self.torreA[i] = " "*self.numeroPiezas
-            self.torreD[i] = " "*self.numeroPiezas
+            self.torreO[i] = " " *(self.numeroPiezas-i-1) + "*"*(2*i+1) + " "*(self.numeroPiezas-i-1)
+            self.torreA[i] = " " * (self.numeroPiezas*2)
+            self.torreD[i] = " " * (self.numeroPiezas*2)
             print(self.torreO[i], " ", self.torreA[i], " ", self.torreD[i])
             
         self.imprimirBase()
+
+
+    def mostrarTorres(self):
+        for i in range (0, self.numeroPiezas):
+            print(self.torreO[i], self.torreA[i], self.torreD[i])
             
+        self.imprimirBase()
+        
     def imprimirBase(self):
         for i  in range (0,2):
             print(("="*(self.numeroPiezas*2-1)), end= " ")
@@ -38,18 +45,61 @@ class Simulacion:
         print(" "*((self.numeroPiezas-1)*2+1), end= "")
         print("A", end= "")
         print(" "*((self.numeroPiezas-1)*2+1), end= "")
-        print("D", end= "")
+        print("D")
 
     def moverPieza(self, origen, destino):
+
+        if self.comprobarPieza(origen, destino):
+            if origen == "O":
+                self.punteroO = self.punteroO+1
+                self.storage = self.torreO[self.punteroO]
+                self.torreO[self.punteroO] = " "*(self.numeroPiezas*2-1)
+            elif origen == "A":
+                self.punteroA = self.punteroA+1
+                self.storage = self.torreA[self.punteroA]
+                self.torreA[self.punteroA] = " "*(self.numeroPiezas*2-1)
+            elif origen == "D":
+                self.punteroD = self.punteroD+1
+                self.storage = self.torreD[self.punteroD]
+                self.torreD[self.punteroD] = " "*(self.numeroPiezas*2-1)
+            if destino == "O":
+                self.torreO[self.punteroO] = self.storage
+                self.punteroO = self.punteroO-1
+            elif destino == "A":
+                self.torreA[self.punteroA] = self.storage
+                self.punteroA = self.punteroA-1
+            elif destino == "D":
+                self.torreD[self.punteroD] = self.storage
+                self.punteroD = self.punteroD-1
+        else:
+            print("La pieza inferior es menor a la superior, por lo que no se puede colocar ahí.")
+
+    def comprobarPieza(self, origen, destino):
+        print(self.punteroO+1)
         if origen == "O":
-            storage = torreO[punteroO]
+            self.piezaSuperior = self.torreO[self.punteroO+1]
         elif origen == "A":
-            storage = torreA[punteroA]
+            self.piezaSuperior = self.torreA[self.punteroA+1]
         elif origen == "D":
-            storage = torreD[punteroD]
-        if destino == "O":
-            torreO[punteroO] = storage
-        elif destino == "A":
-            torreA[punteroA] = storage
-        elif destino == "D":
-            torreD[punteroD] = storage
+            self.piezaSuperior = self.torreD[self.punteroD+1]
+        try:
+            if destino == "O":
+                 self.piezaInferior = self.torreO[self.punteroO+1]
+            elif destino == "A":
+                self.piezaInferior = self.torreA[self.punteroA+1]
+            elif destino == "D":
+                self.piezaInferior = self.torreD[self.punteroD+1]
+        except:
+            self.piezaInferior = " "
+
+        if (self.piezaSuperior.count("*") > self.piezaInferior.count("*")) and self.piezaInferior.count("*") > 0:
+            return False
+        else:
+            return True
+                                        
+    def juegoFinalizado(self):
+        if self.punteroD == -1:
+            return True
+        else:
+            return False
+
